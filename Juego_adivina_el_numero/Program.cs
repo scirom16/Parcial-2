27902ad91ffdf3fc3632babc6f6f8dbd32f17351
scirom <sebastian.cirom16@gmail.com>
2024-04-05 20:12:46 -0500

    Declaración de variables utilizadas en el programa

---
+++ Juego_adivina_el_numero/Program.cs
@@ -0,0 +1,9 @@
+﻿//Declaración de variables
+//Llamar al objeto Random
+Random random = new Random();
+int numeroSecreto = random.Next(1,101);
+int Intento = 0;
+bool adivinado = false;
+int entrada;
+
+//Empezamos a dar la estructura del juego
